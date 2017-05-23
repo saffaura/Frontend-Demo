d76b45a5774e44d59eeff9499ade4a1867256136
Saffaura <bzahid1992@gmail.com>
2017-05-23 10:30:52 -0400

    Modified styling and images

--- src/app/circleProgress/CircleProgressController.js
+++ src/app/circleProgress/CircleProgressController.js
@@ -2,9 +2,9 @@
   angular.module('fancyTasks')
   .controller('CircleProgressController', ['taskService', '$scope', '$rootScope',
   function(taskService, $scope, $rootScope) {
-    $scope.size = 500;
+    $scope.size = 400;
     $scope.complete =  taskService.completed || 0;
-    $scope.color = '#333';
+    $scope.color = {gradient: ["coral", "deeppink", "indigo", "springgreen", "orange"]};
 
     $scope.$on('task:completed', function(event,data) {
       $scope.complete = data;
--- src/app/circleProgress/circleProgressDirective.js
+++ src/app/circleProgress/circleProgressDirective.js
@@ -6,8 +6,17 @@
         controller: 'CircleProgressController',
         templateUrl: 'circleProgress/circleProgress.html',
         link: function (scope, elem, attr) {
-          $('#circle').circleProgress({value: scope.complete});
-          scope.$watchCollection('[complete, size, color]', function() {
+          var color = scope.color ? scope.color : {gradient: ['#ff1e41', '#ff5f43']};
+          var size = scope.size ? scope.size : 200;
+          $('#circle').circleProgress({
+            value: scope.complete,
+            size: size,
+            fill: color,
+            emptyFill: 'rgba(0, 0, 0, 0.42)',
+            thickness: 30
+            });
+          $('#circle').circleProgress('emptyFill', 'gray');
+          scope.$watchCollection('complete', function() {
             $('#circle').circleProgress('value', scope.complete);
           });
         }
